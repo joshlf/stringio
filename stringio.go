49e703bbdc9278fd051367ad8a954674658e5cb4
Joshua Liebow-Feeser <hello@joshlf.com>
2013-08-04 18:12:59 -0400

    Added license and authorship information and comments.

--- stringio.go
+++ stringio.go
@@ -1,3 +1,7 @@
+// Copyright 2013 The Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package stringio provides convenience functions for using strings with
 // common io interfaces such as io.Reader and io.Writer, which normally
 // use byte slices.
